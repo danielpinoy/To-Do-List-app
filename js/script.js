96a2efba63b998e77424c88649f2c8ae4bcafa40
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2023-09-25 14:51:45 +0800

    added list code

---
+++ js/script.js
@@ -0,0 +1,38 @@
+function newItem() {
+    let inputValue = $("#input").val();
+    if (inputValue !== "") {
+        let listItem = $(`<li>${inputValue}</li>`);
+
+        // Close Button Creation
+        let closeButton = $("<button>&times;</button>");
+        closeButton.addClass("exit");
+
+        // Close Button Activate
+        closeButton.on("click", function () {
+            listItem.remove();
+        });
+
+        listItem.on("dblclick", function () {
+            listItem.toggleClass("crossed");
+        });
+        listItem.append(closeButton);
+        $("#list").append(listItem);
+
+        $("#input").val("");
+    } else {
+        alert("Enter Something to Add into the List	");
+    }
+}
+
+// Enter Button
+$("#input").on("keypress", function (event) {
+    if (event.which === 13) {
+        newItem();
+        event.preventDefault();
+    }
+});
+
+// drags list
+$("#list").sortable();
+// text doesn't select text inside list
+$("#list").disableSelection();
